a48a61a298757afc0939eb8df6c008f3f3bd93e5
Megan Dosé <megan.e.dose@gmail.com>
2017-01-22 16:40:14 -0800

    initial commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to '/messages'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to '/messages'
+  end
+
+private
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'welcome' => 'pages#home'
+  get "messages" => "messages#index"
+  get "messages/new" => "messages#new"
+  post "messages" => "messages#create"
+  delete "messages/:id" => "messages#destroy", as: "message_delete"
+end
